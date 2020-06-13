6962f0d3b95add6c8c40df718141002f9336a5c3
satoutakeshi <sato.takeshi1987@gmail.com>
2020-06-13 18:16:23 +0900

    update previewLayer

--- SwiftUI-AVFundation/SwiftUI-AVFundation/Sence/SimpleVideoCapture/SimpleVideoCapturePresenter.swift
+++ SwiftUI-AVFundation/SwiftUI-AVFundation/Sence/SimpleVideoCapture/SimpleVideoCapturePresenter.swift
@@ -10,11 +10,11 @@ import Foundation
 import AVKit
 import Combine
 
-final class SimpleVideoCapturePresenter: NSObject, ObservableObject {
+final class SimpleVideoCapturePresenter: ObservableObject {
 
-    //final class Outputs: ObservableObject {
-        var previewLayer: CALayer = CALayer()
-    //}
+    var previewLayer: CALayer {
+        return interactor.previewLayer!
+    }
 
     enum Inputs {
         case onAppear
@@ -22,86 +22,23 @@ final class SimpleVideoCapturePresenter: NSObject, ObservableObject {
         case onDisappear
     }
 
-    //var outputs: Outputs
-    override init() {
-        super.init()
-        self.prepareCamera()
-        self.beginSession()
+    init() {
+        interactor.setupAVCaptureSession()
     }
 
     func apply(inputs: Inputs) {
         switch inputs {
             case .onAppear:
-               startSession()
+                interactor.startSettion()
             break
             case .tappedCameraButton:
             break
             case .onDisappear:
-              //  interactor.stopSettion()
-                endSession()
-                cancellable?.cancel()
+              interactor.stopSettion()
         }
     }
 
     // MARK: Privates
-    private var cancellable: Cancellable?
-    private let captureSession = AVCaptureSession()
-    private var capturepDevice:AVCaptureDevice!
-    //private let interactor = SimpleVideoCaptureInteractor()
-
-//    private func bind() {
-//        self.cancellable = interactor.$previewLayer.sink(receiveValue: { (previewLayer) in
-//            if let calayer = previewLayer {
-//                self.previewLayer = calayer
-//            }
-//        })
-//    }
-
-    private func prepareCamera() {
-        captureSession.sessionPreset = .photo
-
-        if let availableDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices.first {
-            capturepDevice = availableDevice
-        }
-    }
-    private func beginSession() {
-        do {
-            let captureDeviceInput = try AVCaptureDeviceInput(device: capturepDevice)
-
-            captureSession.addInput(captureDeviceInput)
-        } catch {
-            print(error.localizedDescription)
-        }
-
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        self.previewLayer = previewLayer
-
-        let dataOutput = AVCaptureVideoDataOutput()
-        dataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String:kCVPixelFormatType_32BGRA]
-
-        if captureSession.canAddOutput(dataOutput) {
-            captureSession.addOutput(dataOutput) // -> ここをコメントアウトしたら、カメラボタンタップで画像を取得できなくなった。しかしカメラのライブ映像はそのまま取得できた。
-        }
-
-        captureSession.commitConfiguration()
-
-        let queue = DispatchQueue(label: "FromF.github.com.AVFoundationSwiftUI.AVFoundation")
-        dataOutput.setSampleBufferDelegate(self, queue: queue)
-    }
-
-    func startSession() {
-        if captureSession.isRunning { return }
-        captureSession.startRunning()
-    }
-
-    func endSession() {
-        if !captureSession.isRunning { return }
-        captureSession.stopRunning()
-    }
+    private let interactor = SimpleVideoCaptureInteractor()
 }
 
-extension SimpleVideoCapturePresenter: AVCaptureVideoDataOutputSampleBufferDelegate {
-    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-
-    }
-}
